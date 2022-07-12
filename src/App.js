c0824e61a6aacb6234b085b63d738171074de50f
lydiacho <sheecho0804@ewhain.net>
2022-07-13 00:08:31 +0900

    App.js Route 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import History from './screens/History';
+import Home from './screens/Home';
+import Introduction from './screens/Introduction';
+import Market from './screens/Market';
+import Tutorial from './screens/Tutorial';
 
 function App() {
   return (
-    <div className="App">
-      <h1>GIMME-DUCK</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path = "/" element = { <Home />} />
+        <Route path = "/Introduction" element = { <Introduction />} />
+        <Route path = "/Tutorial" element = { <Tutorial />} />
+        <Route path = "/History" element = { <History />} />
+        <Route path = "/Market" element = { <Market />} />
+      </Routes>
+    </Router>
   );
 }
 
