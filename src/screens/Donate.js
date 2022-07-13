a68b2b3d3beb7df1ceed9c3252958154b56d2a30
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-07-13 23:36:28 +0900

    market page update

---
+++ src/screens/Donate.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Donate = () => {
+  return (
+    <div>기부증서 페이지</div>
+  )
+}
+
+export default Donate
--- src/screens/Market.js
+++ src/screens/Market.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Market = () => {
   return (
-    <div>Market</div>
+    <>
+    <div>
+      <img className='medal' src='img/medal.PNG' />
+    </div>
+    <div style={{height: '100vh'}}>
+      <iframe
+        src="https://testnets.opensea.io/collection/gimmeduck-token"
+        width="100%"
+        height="100%"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
+    </div>
+    </>   
+    
   )
 }
 
