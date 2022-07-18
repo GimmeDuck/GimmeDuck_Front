3a1e40f1bb3c92793366b4e007e33a2114f4d6eb
lydiacho <sheecho0804@ewhain.net>
2022-07-19 00:55:42 +0900

    커스터마이징창 버튼 구현

--- src/screens/Custom.js
+++ src/screens/Custom.js
@@ -10,6 +10,9 @@ const Custom = () => {
             직접 커스터마이징 하지 않고, 랜덤 아이템으로 내 캐릭터를 만듭니다.
             <br/>
             <span style={{color:'lightgoldenrodyellow'}}># 희소성이 높은 아이템이 포함될 수 있습니다!</span> 
+            <div style={{backgroundColor:'red'}}>
+                <button className='cusbutton'>랜덤으로 발행</button>
+            </div>
         </div>
         
         
@@ -18,6 +21,7 @@ const Custom = () => {
             직접 커스터마이징 하지 않고, 랜덤 아이템으로 내 캐릭터를 만듭니다.
             <br/>
             <span style={{color:'lightgoldenrodyellow'}}># 희소성이 높은 아이템이 포함될 수 있습니다!</span> 
+            <button className='cusbutton'>나만의 NFT 발행</button>
         </div>
     </div>
     
