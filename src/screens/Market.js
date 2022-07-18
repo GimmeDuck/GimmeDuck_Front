b0f8ed153eafdb8eafc12c0fa5f0e3bcae54f08c
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-07-19 00:21:34 +0900

    market page 수정하고 donate page 생성

--- src/screens/Market.js
+++ src/screens/Market.js
@@ -4,11 +4,38 @@ import '../style/Market.css'
 const Market = () => {
   return (
     <>
-      <div className='medal'>
-        <div className='second'><img className = 'img' src='img/2.png' /></div>
-        <div className='first'><img className = 'img' src='img/1.png'  /></div>
-        <div className='second'><img className = 'img' src='img/3.png' /></div>
+      <div className='medal_section'>
+
+        <div className='second'>
+          <div className='rapper'>
+            <img className='medal' src = 'img/silver-medal.png'/>
+            <img className = 'img' src='img/2.png' />
+            <p className='nickname'>BTS_duck</p>
+            <p className='klay'>270 Klay</p>
+          </div>        
+        </div>
+
+        <div className='first'>
+          <div className='rapper'>
+            <img className='medal' src = 'img/gold-medal.png'/>
+            <img className = 'img' src='img/1.png'  />
+            <p className='nickname'>IVE_love</p>
+            <p className='klay'>280 Klay</p>
+          </div>
+          
+        </div>
+
+        <div className='second'>
+          <div className='rapper'>
+            <img className='medal' src = 'img/bronze-medal.png'/>
+            <img className = 'img' src='img/3.png' />
+            <p className='nickname'>NCT_jahyun</p>
+            <p className='klay'>250 Klay</p>
+          </div>
+        </div>
+        
       </div>
+
       <div style={{ height: '70vh' }}>
         <iframe
           src="https://testnets.opensea.io/collection/gimmeduck-token"
