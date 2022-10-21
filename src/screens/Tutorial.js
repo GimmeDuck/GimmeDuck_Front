e1e5d60429b07aa25b83eb655f52c9ab55262c69
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-10-22 01:38:11 +0900

    fix : tutorial css

--- src/screens/Tutorial.js
+++ src/screens/Tutorial.js
@@ -27,128 +27,128 @@ const Tutorial = () => {
 
   return (
     <div
-      class="slider_div"
+      class="Tutorial_slider_div"
       style={{ backgroundColor: "black", color: "white" }}
     >
       <Slider {...settings}>
         <div
-          className="container"
+          className="Tutorial_container"
           onMouseOver={() => setIsHover1(100)}
           onMouseOut={() => setIsHover1(0)}
         >
-          <img src="img/tut_1_.png" />
+          <img src="img/tut_1_.png" class="Tutorial_page_img" />
           <div className="hover">
-            <img src="img/tut_1.png" style={{ opacity: isHover1 }} />
+            <img src="img/tut_1.png" class = "Tutorial_hover_img" style={{ opacity: isHover1 }} />
           </div>
         </div>
 
         <div
-          className="container"
+          className="Tutorial_container"
           onMouseOver={() => setIsHover2(100)}
           onMouseOut={() => setIsHover2(0)}
         >
-          <img src="img/tut_2_.png" />
+          <img src="img/tut_2_.png" class="Tutorial_page_img" />
           <div className="hover">
-            <img src="img/tut_2.png" style={{ opacity: isHover2 }} />
+            <img src="img/tut_2.png" class = "Tutorial_hover_img" style={{ opacity: isHover2 }} />
           </div>
         </div>
 
         <div
-          className="container"
+          className="Tutorial_container"
           onMouseOver={() => setIsHover3(100)}
           onMouseOut={() => setIsHover3(0)}
         >
-          <img src="img/tut_3_.png" />
+          <img src="img/tut_3_.png" class="Tutorial_page_img"/>
           <div className="hover">
-            <img src="img/tut_3.png" style={{ opacity: isHover3 }} />
+            <img src="img/tut_3.png" class = "Tutorial_hover_img" style={{ opacity: isHover3 }} />
           </div>
         </div>
 
         <div
-          className="container"
+          className="Tutorial_container"
           onMouseOver={() => setIsHover4(100)}
           onMouseOut={() => setIsHover4(0)}
         >
-          <img src="img/tut_4_.png" />
+          <img src="img/tut_4_.png" class="Tutorial_page_img"/>
           <div className="hover">
-            <img src="img/tut_4.png" style={{ opacity: isHover4 }} />
+            <img src="img/tut_4.png" class = "Tutorial_hover_img" style={{ opacity: isHover4 }} />
           </div>
         </div>
 
         <div
-          className="container"
+          className="Tutorial_container"
           onMouseOver={() => setIsHover5(100)}
           onMouseOut={() => setIsHover5(0)}
         >
-          <img src="img/tut_5_.png" />
+          <img src="img/tut_5_.png" class="Tutorial_page_img" />
           <div className="hover">
-            <img src="img/tut_5.png" style={{ opacity: isHover5 }} />
+            <img src="img/tut_5.png" class = "Tutorial_hover_img" style={{ opacity: isHover5 }} />
           </div>
         </div>
 
         <div
-          className="container"
+          className="Tutorial_container"
           onMouseOver={() => setIsHover6(100)}
           onMouseOut={() => setIsHover6(0)}
         >
-          <img src="img/tut_6_.png" />
+          <img src="img/tut_6_.png" class="Tutorial_page_img"/>
           <div className="hover">
-            <img src="img/tut_6.png" style={{ opacity: isHover6 }} />
+            <img src="img/tut_6.png" class = "Tutorial_hover_img" style={{ opacity: isHover6 }} />
           </div>
         </div>
 
         <div
-          className="container"
+          className="Tutorial_container"
           onMouseOver={() => setIsHover7(100)}
           onMouseOut={() => setIsHover7(0)}
         >
-          <img src="img/tut_7_.png" />
+          <img src="img/tut_7_.png" class="Tutorial_page_img"/>
           <div className="hover">
-            <img src="img/tut_7.png" style={{ opacity: isHover7 }} />
+            <img src="img/tut_7.png" class = "Tutorial_hover_img" style={{ opacity: isHover7 }} />
           </div>
         </div>
 
         <div
-          className="container"
+          className="Tutorial_container"
           onMouseOver={() => setIsHover8(100)}
           onMouseOut={() => setIsHover8(0)}
         >
-          <img src="img/tut_8_.png" />
+          <img src="img/tut_8_.png" class="Tutorial_page_img"/>
           <div className="hover">
-            <img src="img/tut_8.png" style={{ opacity: isHover8 }} />
+            <img src="img/tut_8.png" class = "Tutorial_hover_img" style={{ opacity: isHover8 }} />
           </div>
         </div>
 
         <div
-          className="container"
+          className="Tutorial_container"
           onMouseOver={() => setIsHover9(100)}
           onMouseOut={() => setIsHover9(0)}
         >
-          <img src="img/tut_9_.png" />
+          <img src="img/tut_9_.png" class="Tutorial_page_img"/>
           <div className="hover">
-            <img src="img/tut_9.png" style={{ opacity: isHover9 }} />
+            <img src="img/tut_9.png" class = "Tutorial_hover_img" style={{ opacity: isHover9 }} />
           </div>
         </div>
 
         <div
-          className="container"
+          className="Tutorial_container"
           onMouseOver={() => setIsHover10(100)}
           onMouseOut={() => setIsHover10(0)}
         >
-          <img src="img/tut_10_.png" />
+          <img src="img/tut_10_.png" class="Tutorial_page_img"/>
           <div className="hover">
-            <img src="img/tut_10.png" style={{ opacity: isHover10 }} />
+            <img src="img/tut_10.png" class = "Tutorial_hover_img" style={{ opacity: isHover10 }} />
           </div>
         </div>
 
         <div
-          className="container"
+          className="Tutorial_container"
           onMouseOver={() => setIsHover11(100)}
           onMouseOut={() => setIsHover11(0)}
         >
-          <img src="img/tut_11_.png" />
+          <img src="img/tut_11_.png" class="Tutorial_page_img"/>
           <div className="hover">
-            <img src="img/tut_11.png" style={{ opacity: isHover11 }} />
+            <img src="img/tut_11.png" class = "Tutorial_hover_img" style={{ opacity: isHover11 }} />
           </div>
         </div>
       </Slider>
