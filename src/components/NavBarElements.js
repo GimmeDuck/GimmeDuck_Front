e58e34340f42a646a58636fd5e258ad378a209d4
lydiacho <sheecho0804@ewhain.net>
2022-11-19 20:29:39 +0900

    모바일 접속 제한

--- src/components/NavBarElements.js
+++ src/components/NavBarElements.js
@@ -1,35 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import "../style/NavBar.css";
-
-
-//    <Navbar scrolling dark expand="md" fixed="top" >
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile
+} from "react-device-detect";
 
 
 const NavBarElements = () => {
-
-  // const DEFAULT_QR_CODE = "DEFAULT";
-  // const DEFAULT_ADDRESS = "0x00000000000000000000000000000";
-
-  // const [send_modalIsOpen, send_setModalIsOpen] = useState(false);
-  // const [qrvalue_execute, setQrvalue_execute] = useState(DEFAULT_QR_CODE);
-  // const [myAddress, setMyAddress] = useState(DEFAULT_ADDRESS);
-  // const [duck, setDuck] = useState(0);
-
-  // function wallet_modalOpen() {
-  //   KlipAPI.getAddress(setQrvalue_auth, async (address) => {
-  //     setMyAddress(address);
-  //   });
-  //   auth_setModalIsOpen(true);
-  // }
-
-  // useEffect(() => {
-  //   if (myAddress !== DEFAULT_ADDRESS) {
-  //     // caver js나 klip으로 주소의 balanceOf 가져오기
-  //     console.log(myAddress);
-  //   }
-  // }, [myAddress]);
-
 
   return (
     <Navbar
@@ -60,19 +40,9 @@ const NavBarElements = () => {
         </Nav>
       </Navbar.Collapse>
 
-      {/* <div className="NavBar_box">
-        <button className="NavBar_btn" onClick={() => wallet_modalOpen()}>
-          Klip 지갑연동
-        </button>
-        <Modal className="NavBar_popup" isOpen={send_modalIsOpen}>
-          <QRCodeSVG className="NavBar_qrcode" value={qrvalue_execute} />
-          <div
-            className="NavBar_close"
-            onClick={() => send_setModalIsOpen(false)}
-          ></div>
-        </Modal>
-        <div className="NavBar_text">보유 기미덕 {duck} 개</div>
-      </div> */}
+      <MobileView>
+      </MobileView>
+
     </Navbar>
   );
 };
